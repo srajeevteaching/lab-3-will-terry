16f54e8c6632bcb6b461f1265a4481422d4d1c05
TerrenceObuah <90399261+TerrenceObuah@users.noreply.github.com>
2021-09-30 10:02:39 -0400

    started working on project

---
+++ .github/Lab 3.py
@@ -0,0 +1,11 @@
+weight_of_package=float(input("weight_of_package"))
+float(input("distance of shipping"))
+maxWeight = 20
+minWeight = 0
+minDistance = 10
+maxDistance = 3000
+if(weight_of_package <= minWeight
+    maxWeight < weightPackage)
+print("sorry, cannot ship package")
+
+
