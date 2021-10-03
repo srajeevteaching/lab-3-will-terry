b51e407e48b9ec6a258106c35e385f55edb639b9
Intel76 <hmagan@loyola.edu>
2021-10-03 16:08:42 -0400

    Finish

--- .github/Lab 3.py
+++ .github/Lab 3.py
@@ -1,11 +1,23 @@
-weight_of_package = float(input("weight_of_package"))
-float(input("distance of shipping"))
-maxWeight = 20
-minWeight = 0
-minDistance = 10
-maxDistance = 3000
-if weight_of_package <= minWeight or maxWeight < weight_of_package:
-    print("sorry, cannot ship package")
+# Programmers: [Terrence, Will, Hanna]
+# Course: CS151, Dr. Rajeev
+# Date: Lab Number: 3
+# Program Inputs: [weight, distance]
+# Program Outputs: [charge]
 
-if minDistance < 10 or maxDistance >3000:
-    print(sorry,cannot ship pacakge)
+weight=float(input("please give weight in kg"))
+distance=float(input("please give distance in miles"))
+
+if weight >20 or weight <=0:
+    print("your package either weighs too much or is a negative weight")
+elif distance <10 or distance>3000:
+    print("your distance is either too far or too close")
+elif weight <=2:
+    charge = (distance/500)*1.10
+elif weight <=6:
+    charge = (distance/500)*2.20
+elif weight <=10:
+    charge = (distance/500)*3.70
+elif weight <=20:
+    charge = (distance/500)*4.80
+
+print("your charge is $%.2f dollars" %charge)
